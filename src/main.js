f4badddd7157d87f58ee61c134b3ff27c7d63f36
unknown <grishkovnikola@gmail.com>
2025-12-11 23:11:16 +0200

    add alt

--- src/main.js
+++ src/main.js
@@ -1,31 +1,57 @@
-import { getImagesByQuery } from "./js/pixabay-api.js";
-import { createGallery, clearGallery, showLoader, hideLoader, showNoResults } from "./js/render-functions.js";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from "./pixabay-api.js";
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from "./render-functions.js";
 
 const form = document.querySelector(".form");
 const input = form.querySelector("input[name='search-text']");
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+form.addEventListener("submit", handleSearch);
+
+async function handleSearch(event) {
+  event.preventDefault();
+
   const query = input.value.trim();
 
-  if (!query) return;
+  if (!query) {
+    iziToast.warning({
+      title: "Warning",
+      message: "Будь ласка, введіть ключове слово для пошуку!",
+      position: "topRight",
+    });
+    return;
+  }
 
   clearGallery();
   showLoader();
 
   try {
-    const images = await getImagesByQuery(query);
+    const hits = await getImagesByQuery(query);
 
-    if (images.length === 0) {
-      showNoResults();
-    } else {
-      createGallery(images);
+    if (hits.length === 0) {
+      iziToast.info({
+        title: "Info",
+        message:
+          "На жаль, за вашим запитом нічого не знайдено. Спробуйте інший пошук!",
+        position: "topRight",
+      });
+      return;
     }
+
+    createGallery(hits);
   } catch (error) {
-    console.error(error);
+    iziToast.error({
+      title: "Error",
+      message: error.message,
+      position: "topRight",
+    });
   } finally {
     hideLoader();
   }
-});
-
-iziToast.success({ message: "Бібліотека iziToast працює!" });
+}
